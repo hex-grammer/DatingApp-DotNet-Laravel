6fb5f61558b7748d74f6b10445fe4d77d35fe11e
Rizal Iswandy <rizal@xolusi.com>
2023-08-22 13:15:39 +0800

    End of section 5 (login & register FE)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'client';
   users: any;
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => {
-        this.users = response;
-      },
-      error: (error) => {
-        console.log(error);
-      },
-      complete: () => {
-        console.log('completed');
-      },
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('completed'),
     });
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    const user = userString ? JSON.parse(userString) : null;
+    this.accountService.setCurrentUser(user);
   }
 }
